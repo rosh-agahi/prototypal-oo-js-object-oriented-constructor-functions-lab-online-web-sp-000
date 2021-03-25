12f8ca6cd7d71b01121c8ad8c79e9bf8ad756011
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-25 23:06:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-class Scooter {
-  constructor(year, color, model) {
+function Scooter(year, color, model) {
     this.year = year;
     this.color = color;
     this.model = model
-  }
 }
 
-class Driver {
-  constructor(name, age, experience) {
+function Driver(name, age, experience) {
     this.name = name;
     this.age = age;
     this.experience = experience
-  }
 }
 
-class PickupLocation {
-  constructor(address, city) {
+function PickupLocation(address, city) {
     this.address = address;
     this.city = city;
-  }
+  
 }
